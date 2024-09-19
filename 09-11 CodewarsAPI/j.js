8f91c83be8e193640f41654df87a48449c7bca6a
b0szazal <zalan.szasz06@gmail.com>
2024-09-19 11:54:01 +0200

    Összpontszámot is kiírja

--- 09-11 CodewarsAPI/j.js
+++ 09-11 CodewarsAPI/j.js
@@ -2,6 +2,7 @@ const baseAPI = "https://www.codewars.com/api/v1/users/";
 let users=[];
 let inputElement=document.getElementById("input");
 let content=document.getElementById("content");
+let errorDiv=document.getElementById("errorDiv")
 let output="";
 let searchedUser=null;
 
@@ -28,10 +29,12 @@ async function GetUserData(url){
                 users.push(data);
                 searchedUser=users[users.length-1]
             }
+            errorDiv.innerHTML="";
+            console.log(users)
         }
     }
     catch(error){
-        content.innerHTML="Ilyen név/ID-vel ellátott felhasználó nem létezik\n"+error;
+        errorDiv.innerHTML="Ilyen név/ID-vel ellátott felhasználó nem létezik\n"+error;
     }
 }
 
@@ -40,9 +43,9 @@ function ShowLastUserStats(){
     output+="<h2> Username: "+searchedUser.username+" (id:"+searchedUser.id+")</h2><ol>";
     let userLanguages=searchedUser.ranks.languages;
     for(let [key, value] of Object.entries(userLanguages)){
-        output+="<li>Nyelv: "+key+" Pontszám: "+value.score+"</li>";
+        output+="<li>"+key+" Pontszám: "+value.score+"</li>";
     }
-    output+="</ol>"
+    output+="</ol><h3>Összesen: "+searchedUser.ranks.overall.score+"</h3>"
     content.innerHTML=output;
 }
 
