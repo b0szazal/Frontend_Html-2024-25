3e1c2fd5b3b41942280ac5cfabcb761a0be29b5a
b0szazal <b0szazal@vasvari.org>
2025-02-14 15:38:04 +0100

    updatePlant inputs have values of selectedPlant

--- 02-14/frontend/vue-kerteszet/src/views/updatePlantPage.vue
+++ 02-14/frontend/vue-kerteszet/src/views/updatePlantPage.vue
@@ -7,7 +7,7 @@
                         <label for="nev">Növény neve</label>
                     </td>
                     <td>
-                        <input type="text" name="nev" id="nev" v-model="nev">
+                        <input type="text" name="nev" id="nev" v-model="selectedPlant.nev">
                     </td>
                 </tr>
                 <tr>
@@ -15,7 +15,7 @@
                         <label for="evelo-e">Évelő-e</label>
                     </td>
                     <td>
-                        <input type="checkbox" name="evelo-e" id="evelo-e" v-model="eveloE">
+                        <input type="checkbox" name="evelo-e" id="evelo-e" v-model="selectedPlant.eveloE">
                     </td>
                 </tr>
                 <tr>
@@ -23,7 +23,7 @@
                         <label for="kategoria">Kategória</label>
                     </td>
                     <td>
-                        <input type="text" name="kategoria" id="kategoria" v-model="kategoria">
+                        <input type="text" name="kategoria" id="kategoria" v-model="selectedPlant.kategoria">
                     </td>
                 </tr>
                 <tr>
@@ -31,7 +31,7 @@
                         <label for="ar">Ár:</label>
                     </td>
                     <td>
-                        <input type="number" name="ar" id="ar" placeholder="Ft" v-model="ar">
+                        <input type="number" name="ar" id="ar" placeholder="Ft" v-model="selectedPlant.ar">
                     </td>
                 </tr>
             </tbody>
@@ -50,19 +50,15 @@ import {  useRouter } from 'vue-router';
 
 const router = useRouter()
 const plantStore = usePlantStore();
-
-const nev = ref("");
-const eveloE = ref(false)
-const kategoria = ref("");
-const ar=ref()
+const selectedPlant = plantStore.selectedPlant;
 
 const SavePlant= async ()=>{
     let json={};
-    json.nev=nev.value;
-    json.eveloE=eveloE.value;
-    json.kategoria=kategoria.value;
-    json.ar=ar.value;
-    await plantStore.updatePlant(plantStore.selectedPlant.id, json);
+    json.nev=selectedPlant.nev;
+    json.eveloE=selectedPlant.eveloE;
+    json.kategoria=selectedPlant.kategoria;
+    json.ar=selectedPlant.ar;
+    await plantStore.updatePlant(selectedPlant.id, json);
     router.push("/")
 }
 
