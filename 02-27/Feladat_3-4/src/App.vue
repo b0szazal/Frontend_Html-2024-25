7fc3ecfdedf7caa6748f6b34336b0615b8413c3a
b0szazal <zalan.szasz06@gmail.com>
2025-02-28 12:48:02 +0100

    működik

--- 02-27/Feladat_3-4/src/App.vue
+++ 02-27/Feladat_3-4/src/App.vue
@@ -2,8 +2,52 @@
   <header>
     <div class="wrapper">
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/" class="fs-5">Home</RouterLink>
       </nav>
+    </div>
+    <div class="container">
+      <div class="get">
+        <div>
+          <label for="fourInput">Számnégyes vesszővel elválasztva</label>
+          <input type="text" id="fourInput" placeholder="minta: 1,1,1,1" v-model="four">
+          <button @click="CheckForErrors()">Generálás</button>
+          <p v-text="fourError"></p>
+          <div v-if="!exists">
+              <button @click="fourStore.postFour(four)">Számnégyes mentése</button>
+          </div>
+        </div>
+        <div>
+          <label for="id">Id alapján lekérdezés:</label>
+          <input type="text" id="id" v-model="fourId" placeholder="pl. 1">
+          <button @click="FindFour()">Lekérdezés</button>
+        </div>
+      </div>
+      <div class="container text-center bg-primary fs-4 rounded-4">
+        <div class="row p-5">
+              <div class="col-4" v-text="table[0][0]">
+              </div>
+              <div class="col-4" v-text="table[0][1]">
+              </div>
+              <div class="col-4" v-text="table[0][2]">
+              </div>
+          </div>
+          <div class="row p-5">
+              <div class="col-4" v-text="table[1][0]">
+              </div>
+              <div class="col-4" v-text="table[1][1]">
+              </div>
+              <div class="col-4" v-text="table[1][2]">
+              </div>
+          </div>
+          <div class="row p-5">
+              <div class="col-4" v-text="table[2][0]">
+              </div>
+              <div class="col-4" v-text="table[2][1]">
+              </div>
+              <div class="col-4" v-text="table[2][2]">
+              </div>
+          </div>
+      </div>
     </div>
   </header>
 
@@ -11,7 +55,51 @@
 </template>
 
 <script setup>
-  import { RouterLink, RouterView } from 'vue-router'
+  import { ref } from 'vue';
+import { RouterLink, RouterView } from 'vue-router'
+import { useFoursStore } from './stores/fours';
+
+  const fourStore=useFoursStore();
+
+  const four= ref('');
+  const fourError = ref('');
+  const fourId = ref('');
+  const table= ref([[0, 0, 0], [0, 0, 0], [0, 0, 0]]);
+  const exists = ref(true);
+
+  const CheckForErrors = () => {
+    let numbers = four.value.split(',');
+    if(numbers.length != 4){
+      fourError.value = 'Nem 4 db szám lett megadva!';
+      return;
+    }
+    for(let i=0; i<4; i++){
+      if(isNaN(numbers[i])){
+        fourError.value = 'Nem számot adtál meg!';
+        return;
+      }
+    }
+    Generate(numbers);
+  }
+
+  const Generate = async (numbers)=>{
+    if(numbers.length != 4){
+      fourError.value = numbers;
+      return;
+    }
+    numbers = numbers.map(Number);
+    table.value= [
+      [numbers[0], numbers[0]+numbers[1], numbers[1]],
+      [numbers[0]+numbers[2], numbers[0]+numbers[1]+numbers[2]+numbers[3], numbers[1]+numbers[3]],
+      [numbers[2], numbers[2]+numbers[3], numbers[3]]
+    ]
+
+    exists.value = await fourStore.CheckIfExist(numbers);
+  }
+
+  const FindFour = async () => {
+    Generate(await fourStore.FindFour(fourId.value))
+  }
 </script>
 
 <style scoped>
@@ -32,21 +120,22 @@ nav {
   margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.fs-5{
+  font-size: xx-large;
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.container{
+  max-width: 80%;
+  margin: auto;
 }
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.get{
+  display: flex;
+  max-width: fit-content;
+  margin: auto;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 2rem;
 }
-
-nav a:first-of-type {
-  border: 0;
+input{
+  margin: 25px;
 }
 </style>
--- 02-27/Feladat_3-4/src/stores/fours.js
+++ 02-27/Feladat_3-4/src/stores/fours.js
@@ -1,8 +1,69 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import axios from 'axios';
+
+let url="http://localhost:3000/fours/"
 
 export const useFoursStore = defineStore('fours', () => {
-  const four = ref([0, 0, 0, 0]);
+  const FindFour = async (id)=>{
+    let res=""
+    try{
+      await axios.get(url+id)
+      .then((response)=>{
+        res= response.data.data
+      })
+    }
+    catch(err){
+      res= err
+    }
 
-  return { four }
+    return res;
+  }
+
+  const CheckIfExist = async (corners)=>{
+    let fours;
+    let exists=false;
+    await axios.get(url)
+    .then((response)=>{
+      fours= response.data
+    })
+    .then(()=>{
+      for(let i=0; i<fours.length; i++){
+        if(arraysEqual(corners, fours[i].data)){
+          exists=true;
+        }
+      }
+    })
+    console.log(exists)
+    return exists;
+  }
+
+  function arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;  
+    for (var i = 0; i < a.length; ++i) {
+      if (a[i] !== b[i]) return false;
+    }
+    return true;
+  }
+
+  const postFour = async (four) =>{
+    let numbers = four.split(',');
+    numbers = numbers.map(Number);
+    let res=""
+    try{
+      await axios.post(url, {data: numbers})
+      .then(()=>{
+        res= "sikeres mentés"
+      })
+    }
+    catch(err){
+      res= err
+    }
+
+    return res;
+  }
+
+  return {FindFour , CheckIfExist, postFour}
 })
